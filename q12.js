a63befd6eee4ee7ecee932dbdcc98731d7141c67
Tarannum10 <tarannum10samad@gmail.com>
2024-12-29 04:34:46 +0530

    q12

---
+++ q12.js
@@ -0,0 +1,6 @@
+let task=["wake up early","brush your teeth","take a bath","dress properly","get ready"]
+let more = "snooze the alarm"
+task.unshift(more)
+// console.log(task)
+task.pop()
+console.log(task)
