f2744811833bace8263639895f46fb62ba84ec1f
Tarannum10 <tarannum10samad@gmail.com>
2024-12-29 19:41:50 +0530

    q14

---
+++ q14.js
@@ -0,0 +1,5 @@
+let arr=["clean the house", "wash the utensils", "fill water bottles", "look over other things", "take care of family"]
+arr.shift()
+arr.unshift("change the bedsheets", "iron the clothes")
+arr[arr.length-1]="cut your nails"
+console.log(arr)
